fa86aff34da1e4213801cb941f89fbc9975fca79
shinchan <akashkumar3432@gmail.com>
2020-08-18 09:49:26 -0700

    Updates

---
+++ precache-manifest.7f1486b6de3a709b7067f3b3806f3e35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b604ba6e713ea81cce0a48018c553f",
+    "url": "/collegeDuniaTask/index.html"
+  },
+  {
+    "revision": "cde7ab8d9c86ba9f98db",
+    "url": "/collegeDuniaTask/static/css/main.a36eda0c.chunk.css"
+  },
+  {
+    "revision": "3d2adbfb29c61bc49f3d",
+    "url": "/collegeDuniaTask/static/js/2.fa7ae0e6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/collegeDuniaTask/static/js/2.fa7ae0e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde7ab8d9c86ba9f98db",
+    "url": "/collegeDuniaTask/static/js/main.16097ce2.chunk.js"
+  },
+  {
+    "revision": "b90d96ac1d65d44ae8cd",
+    "url": "/collegeDuniaTask/static/js/runtime-main.fd85eadd.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/collegeDuniaTask/static/media/college_02.619ee97e.jpg"
+  }
+]);
